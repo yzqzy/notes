f3288068d6c7b8bfe1145a9e3cbc08cb78b80ae0
heora <yueluo.yang@qq.com>
2021-06-17 08:26:29 +0800

    feat: 0-1 背包问题

--- algorithm_google/recall/index.js
+++ algorithm_google/recall/index.js
@@ -59,3 +59,33 @@ function printQueens (result) {
 }
 
 cal8queens(0);
+
+
+
+
+
+
+
+
+
+// 背包
+const maxN = Number.MAX_SAFE_INTEGER; // 存储背包中物品总重量的最大值
+// cw 表示当前已经装进去的物品的重量和；i 表示考察到哪个物品了；
+// w 背包重量；items 表示每个物品的重量； n表示物品个数
+// 假设背包可承受重量 100，物品个数 10，物品重量存储在数组 a 中，那可以这样调用函数：
+// f(0, 0, a, 10, 100)
+
+function f (i, cw, items, n, w) {
+  if (cw == w || i == n) {
+    // cw == w 表示装满了; i == n 表示已经考察完所有的物品
+    if (cw > maxN) maxN = cw;
+    return;
+  }
+
+  f(i + 1, cw, items, n, w);
+
+  if (cw + items[i] <= w) {
+    // 已经超过可以背包承受的重量的时候，就不要再装了
+    f(i + 1, cw + items[i], items, n, w);
+  }
+}
