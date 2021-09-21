f488c1a605a9490776996751bc7e4a8a0c8c6f55
yueluo <yueluo.yang@qq.com>
2021-09-22 07:58:16 +0800

    perf: 实现 fiber 的调度逻辑

--- react/Fiber/src/react/misc/createTaskQueue/index.js
+++ react/Fiber/src/react/misc/createTaskQueue/index.js
@@ -4,5 +4,6 @@ export default function createTaskQueue () {
   return {
     push: item => taskQueue.push(item),
     pop: () => taskQueue.shift(),
+    isEmpty: () => taskQueue.length === 0
   };
 }
--- react/Fiber/src/react/reconciliation/index.js
+++ react/Fiber/src/react/reconciliation/index.js
@@ -1,6 +1,35 @@
 import { createTaskQueue } from "../misc"
 
 const taskQueue = createTaskQueue();
+let subTask = null;
+
+const getFirstTask = () => {
+
+}
+
+const executeTask = fiber => {
+  
+}
+
+const workLoop = deadline => {
+  if (!subTask) {
+    subTask = getFirstTask();
+  }
+
+  // 如果任务存在并且浏览器有空闲时间就调用
+  while (subTask && deadline.timeRemaining() > 1) {
+    subTask = executeTask(subTask);
+  }
+}
+
+const performTask = deadline => {
+  // 执行任务
+  workLoop(deadline);
+  // 如果任务是否存在
+  if (subTask || !taskQueue.isEmpty()) {
+    requestIdleCallback(performTask);
+  }
+}
 
 export const render = (element, dom) => {
   // 1. 向任务队列中添加任务
@@ -9,6 +38,6 @@ export const render = (element, dom) => {
     props: { children: element }
   });
   
-
   // 2. 指定在浏览器空闲时执行任务
+  requestIdleCallback(performTask);
 }
