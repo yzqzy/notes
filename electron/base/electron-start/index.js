3f080c683492485d6c9482acdd174025dca9b3d6
yueluo <yueluo.yang@qq.com>
2021-10-20 07:59:53 +0800

    perf: 注册全局快捷键

--- electron/base/electron-start/index.js
+++ electron/base/electron-start/index.js
@@ -1,17 +1,3 @@
 window.addEventListener('DOMContentLoaded', () => {
   const oBtn = document.getElementById('J-btn');
-
-  oBtn.addEventListener('click', () => {
-    const options = {
-      title: '提示',
-      body: '消息通知',
-      icon: './favicon.ico',
-    };
-
-    const notification = new window.Notification(options.title, options);
-
-      notification.onclick = () => {
-        console.log('click');
-      }
-  });
 });
--- electron/base/electron-start/main.js
+++ electron/base/electron-start/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
 
 let mainWinId = null
 
@@ -27,6 +27,23 @@ function createWindow () {
 }
 
 app.whenReady().then(createWindow);
+
+app.on('ready', () => {
+  const ret = globalShortcut.register('ctrl + q', () => {
+    console.log('222')
+  });
+
+  if (!ret) {
+    console.log('girst shortcut failed.');
+  }
+
+  console.log(globalShortcut.isRegistered('ctrl + q'));
+});
+
+app.on('will-quit', () => {
+  // globalShortcut.unregister('ctrl + q');
+  globalShortcut.unregisterAll();
+});
 
 app.on('window-all-closed', () => {
   app.quit();
