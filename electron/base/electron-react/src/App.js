63cfba7202153eb8418825a21667fb4e4f10097b
yueluo <yueluo.yang@qq.com>
2021-10-20 22:28:11 +0800

    perf: 引入 bootstrap 与 styled-components

--- electron/base/electron-react/src/App.js
+++ electron/base/electron-react/src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const LeftBoard = styled.div.attrs({
+  className: 'col-3 left-panel',
+})`
+  background-color: #7b8c7c;
+  min-height: 100vh;
+`;
+
+const RightBoard = styled.div.attrs({
+  className: 'col-9 left-panel'
+})`
+  background-color: #c9d8cd;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h2>Markdown 编辑器</h2>
+    <div className="App container-fluid">
+      <div className="row no-gutters">
+        <LeftBoard>左侧</LeftBoard>
+        <RightBoard>右侧</RightBoard>
+      </div>
     </div>
   );
 }
