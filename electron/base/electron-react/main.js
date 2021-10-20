de2368c023477617cf84a9650d8ce6008ed0ae5e
yueluo <yueluo.yang@qq.com>
2021-10-20 22:11:43 +0800

    perf: react with electron

---
+++ electron/base/electron-react/main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+
+let mainWindow = null;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 650,
+    minWidth: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true
+    }
+  });
+
+  const urlLocation = isDev ? 'http://localhost:3000' : ''
+
+  mainWindow.loadURL(urlLocation);
+});
---
+++ electron/base/electron-react/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Markdown 编辑器</h2>
+    </div>
+  );
+}
+
+export default App;
