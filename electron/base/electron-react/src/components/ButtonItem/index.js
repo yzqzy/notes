a8fec21e086cae22bfb402b6fe139439c0a0c880
yueluo <yueluo.yang@qq.com>
2021-10-24 08:09:21 +0800

    perf: update

--- electron/base/electron-react/src/components/ButtonItem/index.js
+++ electron/base/electron-react/src/components/ButtonItem/index.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import PropTypes from 'prop-types';
 
 const BtnP = styled.p.attrs({
   className: 'btn no-border'
