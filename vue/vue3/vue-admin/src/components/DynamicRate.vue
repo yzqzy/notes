2f45bb4b18853e0c244186b9c23a2d3848e25ee9
yueluo <yueluo.yang@qq.com>
2022-05-14 19:52:55 +0800

    feat(vue3): update

---
+++ vue/vue3/vue-admin/src/components/DynamicRate.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div :style="fontstyle">
+    {{ rate }}
+  </div>
+</template>
+
+<script setup>
+import { computed, } from 'vue';
+
+const props = defineProps({
+  value: Number,
+  theme: { type: String, default: 'orange' }
+})
+const rate = computed(() => "★★★★★☆☆☆☆☆".slice(5 - props.value, 10 - props.value))
+
+const themeObj = {
+  'black': '#00',
+  'white': '#fff',
+  'red': '#f5222d',
+  'orange': '#fa541c',
+  'yellow': '#fadb14',
+  'green': '#73d13d',
+  'blue': '#40a9ff',
+}
+
+const fontstyle = computed(() => {
+  return `color:${themeObj[props.theme]};`
+})
+</script>
--- vue/vue3/vue-admin/src/pages/home.vue
+++ vue/vue3/vue-admin/src/pages/home.vue
@@ -1,14 +1,21 @@
 
 <template>
   <h1>这是首页</h1>
-  
+
   <TodoList />
   <Counetr />
+  <Rate :value="3"></Rate>
+  <Rate :value="4" theme="red"></Rate>
+  <Rate :value="1" theme="green"></Rate>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import TodoList from '../components/TodoList.vue';
 import Counetr from '../components/Counetr.vue';
+import Rate from '../components/Rate.vue';
+
+const score = ref(3);
 </script>
 
 <style scoped>
