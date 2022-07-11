829e366ef1a659f1a005fb7a373f1c077883ed58
yueluo <yueluo.yang@qq.com>
2022-07-11 23:05:38 +0800

    feat(vue3): named character reference parser

--- books/Vue.js设计与实现/compiler/index13.js
+++ books/Vue.js设计与实现/compiler/index13.js
@@ -254,11 +254,37 @@ function parseAttributes(context) {
   return props
 }
 
-function parseText() {
+function parseText(context) {
+  // endIndex 为文本内容得结尾索引，默认将整个模板剩余内容都作为文本内容
+  let endIndex = context.source.length
+  // 寻找字符 < 的位置索引
+  const ltIndex = context.source.indexOf('<')
+  // 寻找定界符 {{ 的位置索引
+  const delimiterIndex = context.source.indexOf('{{')
 
+  // 取 ltIndex 和当前 endIndex 中较小的一个作为新的结尾索引
+  if (ltIndex > -1 && ltIndex < endIndex) {
+    endIndex = ltIndex
+  }
+  // 取 delimiterIndex 和当前 endIndex 中较小的一个作为新的结尾索引
+  if (delimiterIndex > -1 && delimiterIndex < endIndex) {
+    endIndex = delimiterIndex
+  }
+
+  // 此时 endIndex 是最终的文本内容的结尾索引，调用 slice 函数截取文本内容
+  const content = context.source.slice(0, endIndex)
+  // 消耗文本内容
+  context.advanceBy(content.length)
+
+  // 返回文本节点
+  return {
+    // 节点类型
+    type: 'Text',
+    // 文本内容
+    content
+  }
 }
 
-// const ast = parse('<div id="foo" v-show="display"></div>')
-const ast = parse('<div :id="dynamicId" @click="handler" v-on:mousedown="onMouseDown"></div>')
+const ast = parse('<div>Text</div>')
 
 console.log(ast)
---
+++ books/Vue.js设计与实现/compiler/index14.js
@@ -0,0 +1,11 @@
+const namedCharacterReferences = {
+  "gt": ">",
+  "gt;": ">",
+  "lt": "<",
+  "lt;": "<",
+  "ltcc;": "⪦"
+}
+
+function decodeHtml(rawText, asAttr = false) {
+  
+}
