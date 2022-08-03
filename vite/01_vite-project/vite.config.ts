efc781736656da7ddfc08ea9968637d6ae361c75
heora <yangzhiqiang@winwin-inc.com>
2022-08-03 10:19:52 +0800

    perf(vite): adjust project name

--- vite/01_vite-project/vite.config.ts
+++ vite/01_vite-project/vite.config.ts
@@ -8,6 +8,7 @@ const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 指定项目根目录位置
   // root: path.join(__dirname, 'src'),
   css: {
     preprocessorOptions: {
