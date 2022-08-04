cc3e66de4c8bc8884b21fb3a48a9413c443f2da5
heora <yangzhiqiang@winwin-inc.com>
2022-08-04 17:29:26 +0800

    chore(docs): modify website title

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,7 @@ import sidebars from './sidebar.json'
 export default defineConfig((ctx) => ({
   base: ctx.mode == 'production' ? '/notes/' : '', 
   lang: 'en-US',
-  title: '月落 - Personal Notes',
+  title: 'Personal Notes',
   head: [
     ['link', { rel: 'icon', href: '/notes/favicon.ico' }]
   ],
