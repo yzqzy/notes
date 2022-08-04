9ab4d4d9cab9fbdadc0223bc5c25975d9290f6cf
heora <yangzhiqiang@winwin-inc.com>
2022-08-04 17:27:06 +0800

    chore(docs): add soicalLinks

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -11,9 +11,10 @@ export default defineConfig((ctx) => ({
   ],
   description: 'Web Developer & JS Fancier',
   themeConfig: {
+    sidebar: sidebars as any,
+    socialLinks: [{ icon: "github", link: "https://github.com/yw0525" }],
     footer: {
       copyright: 'Copyright © 2022-月落 版权所有'
-    },
-    sidebar: sidebars as any
+    }
   }
 }))
