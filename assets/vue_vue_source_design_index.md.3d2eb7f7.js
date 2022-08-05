f88a6c0af1b59eb1755bb6253cfd7cb88b1000f2
yw0525 <yw0525@users.noreply.github.com>
2022-08-05 15:22:24 +0000

    deploy: debf030855a27239fffd1827d29f208290622812

---
+++ assets/vue_vue_source_design_index.md.3d2eb7f7.js
@@ -0,0 +1,259 @@
+import{_ as e,c as n,o as t,a as d}from"./app.cf466274.js";const w=JSON.parse('{"title":"Vue\u6838\u5FC3\u6E90\u7801\u5B9E\u73B0","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u73AF\u5883\u914D\u7F6E\u3001\u57FA\u7840\u4EE3\u7801\u7F16\u5199","slug":"\u73AF\u5883\u914D\u7F6E\u3001\u57FA\u7840\u4EE3\u7801\u7F16\u5199"},{"level":2,"title":"\u5BF9\u8C61\u52AB\u6301\u3001\u8BBF\u95EE\u5C5E\u6027\u4EE3\u7406","slug":"\u5BF9\u8C61\u52AB\u6301\u3001\u8BBF\u95EE\u5C5E\u6027\u4EE3\u7406"},{"level":2,"title":"\u6570\u7EC4\u52AB\u6301","slug":"\u6570\u7EC4\u52AB\u6301"},{"level":2,"title":"\u7F16\u8BD1\u6587\u672C","slug":"\u7F16\u8BD1\u6587\u672C"},{"level":2,"title":"\u4F9D\u8D56\u6536\u96C6","slug":"\u4F9D\u8D56\u6536\u96C6"},{"level":2,"title":"\u6279\u91CF\u5F02\u6B65\u66F4\u65B0\u7B56\u7565","slug":"\u6279\u91CF\u5F02\u6B65\u66F4\u65B0\u7B56\u7565"},{"level":2,"title":"\u6570\u7EC4\u7684\u4F9D\u8D56\u6536\u96C6","slug":"\u6570\u7EC4\u7684\u4F9D\u8D56\u6536\u96C6"},{"level":2,"title":"\u5B9A\u4E49watch\u65B9\u6CD5","slug":"\u5B9A\u4E49watch\u65B9\u6CD5"},{"level":2,"title":"\u8BA1\u7B97\u5C5E\u6027\u7684\u5B9E\u73B0","slug":"\u8BA1\u7B97\u5C5E\u6027\u7684\u5B9E\u73B0"},{"level":2,"title":"\u8BA1\u7B97\u5C5E\u6027\u66F4\u65B0\u95EE\u9898","slug":"\u8BA1\u7B97\u5C5E\u6027\u66F4\u65B0\u95EE\u9898"},{"level":2,"title":"\u4E86\u89E3\u865A\u62DFDOM","slug":"\u4E86\u89E3\u865A\u62DFdom"},{"level":2,"title":"\u5C06\u865A\u62DFDOM\u8F6C\u6362\u4E3A\u771F\u5B9EDOM","slug":"\u5C06\u865A\u62DFdom\u8F6C\u6362\u4E3A\u771F\u5B9Edom"},{"level":2,"title":"\u8282\u70B9\u66F4\u65B0\u65F6\uFF0C\u865A\u62DFDOM\u5904\u7406","slug":"\u8282\u70B9\u66F4\u65B0\u65F6\uFF0C\u865A\u62DFdom\u5904\u7406"},{"level":2,"title":"\u8001\u8282\u70B9\u6709\u5B50\u8282\u70B9\uFF0C\u65B0\u8282\u70B9\u4E5F\u6709\u5B50\u8282\u70B9\u7684\u60C5\u51B5","slug":"\u8001\u8282\u70B9\u6709\u5B50\u8282\u70B9\uFF0C\u65B0\u8282\u70B9\u4E5F\u6709\u5B50\u8282\u70B9\u7684\u60C5\u51B5"},{"level":2,"title":"\u5904\u7406\u65B0\u5143\u7D20\u5728\u5934\u90E8\u6216\u5143\u7D20\u5012\u5E8F\u95EE\u9898","slug":"\u5904\u7406\u65B0\u5143\u7D20\u5728\u5934\u90E8\u6216\u5143\u7D20\u5012\u5E8F\u95EE\u9898"},{"level":2,"title":"\u865A\u62DFDOM\u4EE3\u7801\u5408\u5E76\u5230\u539F\u6709vue\u903B\u8F91\u4E2D\uFF08\u5408\u5E76Vue\u4EE3\u7801)","slug":"\u865A\u62DFdom\u4EE3\u7801\u5408\u5E76\u5230\u539F\u6709vue\u903B\u8F91\u4E2D\uFF08\u5408\u5E76vue\u4EE3\u7801"}],"relativePath":"vue/vue_source_design/index.md"}'),r={name:"vue/vue_source_design/index.md"},a=d(`<h1 id="vue\u6838\u5FC3\u6E90\u7801\u5B9E\u73B0" tabindex="-1">Vue\u6838\u5FC3\u6E90\u7801\u5B9E\u73B0 <a class="header-anchor" href="#vue\u6838\u5FC3\u6E90\u7801\u5B9E\u73B0" aria-hidden="true">#</a></h1><h2 id="\u73AF\u5883\u914D\u7F6E\u3001\u57FA\u7840\u4EE3\u7801\u7F16\u5199" tabindex="-1">\u73AF\u5883\u914D\u7F6E\u3001\u57FA\u7840\u4EE3\u7801\u7F16\u5199 <a class="header-anchor" href="#\u73AF\u5883\u914D\u7F6E\u3001\u57FA\u7840\u4EE3\u7801\u7F16\u5199" aria-hidden="true">#</a></h2><p>npm init</p><p>npm i webpack webpack-cli webpack-dev-server --save-dev</p><p>npm i html-webpack-plugin --save-dev</p><h2 id="\u5BF9\u8C61\u52AB\u6301\u3001\u8BBF\u95EE\u5C5E\u6027\u4EE3\u7406" tabindex="-1">\u5BF9\u8C61\u52AB\u6301\u3001\u8BBF\u95EE\u5C5E\u6027\u4EE3\u7406 <a class="header-anchor" href="#\u5BF9\u8C61\u52AB\u6301\u3001\u8BBF\u95EE\u5C5E\u6027\u4EE3\u7406" aria-hidden="true">#</a></h2><h2 id="\u6570\u7EC4\u52AB\u6301" tabindex="-1">\u6570\u7EC4\u52AB\u6301 <a class="header-anchor" href="#\u6570\u7EC4\u52AB\u6301" aria-hidden="true">#</a></h2><p>vue\u4E2D\u6570\u7EC4\u52AB\u6301\u7684\u7F3A\u70B9</p><pre><code>1. \u76F4\u63A5\u901A\u8FC7\u6570\u7EC4\u7D22\u5F15\u6539\u53D8\u503C\uFF0C\u662F\u4E0D\u4F1A\u88AB\u89C2\u5BDF\u5230\u7684\u3002
+
+  [1, 3, 4] =&gt; vm.nums[2] = 5;  
+
+2. \u6570\u7EC4\u957F\u5EA6\u4FEE\u6539\uFF0C\u4E0D\u4F1A\u5BF9\u5176\u89C2\u5BDF
+</code></pre><h2 id="\u7F16\u8BD1\u6587\u672C" tabindex="-1">\u7F16\u8BD1\u6587\u672C <a class="header-anchor" href="#\u7F16\u8BD1\u6587\u672C" aria-hidden="true">#</a></h2><p>\u6839\u636E\u6570\u636E\u8FDB\u884C\u9875\u9762\u6E32\u67D3\uFF0C\u533A\u5206\u5143\u7D20\u8282\u70B9\u548C\u6587\u672C\u8282\u70B9\uFF0C\u5E76\u8FDB\u884C\u7F16\u8BD1\u3002</p><h2 id="\u4F9D\u8D56\u6536\u96C6" tabindex="-1">\u4F9D\u8D56\u6536\u96C6 <a class="header-anchor" href="#\u4F9D\u8D56\u6536\u96C6" aria-hidden="true">#</a></h2><p>\u6570\u636E\u9A71\u52A8\u89C6\u56FE\u66F4\u6539\u3002</p><p>\u89E3\u51B3\u8BA2\u9605\u4E8B\u4EF6\u91CD\u590D\u7684\u95EE\u9898\uFF0C\u4F7Fdep\u548Cwatcher\u76F8\u4E92\u4F9D\u8D56\u3002</p><h2 id="\u6279\u91CF\u5F02\u6B65\u66F4\u65B0\u7B56\u7565" tabindex="-1">\u6279\u91CF\u5F02\u6B65\u66F4\u65B0\u7B56\u7565 <a class="header-anchor" href="#\u6279\u91CF\u5F02\u6B65\u66F4\u65B0\u7B56\u7565" aria-hidden="true">#</a></h2><p>Promise\u3001MutationObserver \u5FAE\u4EFB\u52A1 setImmediate\u3001setTimeout \u5B8F\u4EFB\u52A1</p><p>\u5FAE\u4EFB\u52A1\u6BD4\u5B8F\u4EFB\u52A1\u6D88\u8017\u7684\u65F6\u95F4\u77ED\uFF0C\u652F\u6301Promise\u7684\u8BDD\uFF0C\u53EF\u4EE5\u66F4\u5FEB\u7684\u6267\u884C\u7A0B\u5E8F\u3002</p><p>/**</p><ul><li>@description \u6267\u884C\u5F02\u6B65\u51FD\u6570</li><li>@param {function} cb - \u56DE\u8C03\u51FD\u6570</li><li>@return {void} */ function nextTick (cb) { callbacks.push(cb);</li></ul><pre><code>let timerFunction = () =&gt; {
+  flushCallbacks();
+}
+
+if (Promise) {
+  return Promise.resolve().then(timerFunction);
+}
+
+if (MutationObserver) {
+  // html5\u4E2D\u7684API
+  let observe = new MutationObserver(timerFunction);
+  let textNode = document.createElement(10);
+  observe.observe(textNode, { characterData: true });
+  textNode.textContent = 20;
+  return;
+}
+
+if (setImmediate) {
+  // \u6027\u80FD\u6BD4setTimeout\u597D\uFF0C\u8001\u7248\u672C\u4E0D\u652F\u6301
+  return setImmediate(timerFunction);
+}
+
+// \u5FC5\u9009\u9879
+setTimeout(timerFunction);
+</code></pre><p>}</p><h2 id="\u6570\u7EC4\u7684\u4F9D\u8D56\u6536\u96C6" tabindex="-1">\u6570\u7EC4\u7684\u4F9D\u8D56\u6536\u96C6 <a class="header-anchor" href="#\u6570\u7EC4\u7684\u4F9D\u8D56\u6536\u96C6" aria-hidden="true">#</a></h2><p>\u6570\u7EC4\u548C\u591A\u7EF4\u6570\u7EC4\u9700\u8981\u5355\u72EC\u5904\u7406\u4F9D\u8D56\u6536\u96C6\u3002</p><h2 id="\u5B9A\u4E49watch\u65B9\u6CD5" tabindex="-1">\u5B9A\u4E49watch\u65B9\u6CD5 <a class="header-anchor" href="#\u5B9A\u4E49watch\u65B9\u6CD5" aria-hidden="true">#</a></h2><p>\u6BCF\u4E00\u4E2Awatch\u90FD\u662F\u4E00\u4E2Awatcher\u3002</p><p>vue\u539F\u578B\u4E0A\u6302\u8F7Dwatch\u65B9\u6CD5\uFF0C\u5B9E\u4F8B\u5316\u4E00\u4E2Awatcher</p><p>Vue.prototype.$watch = function (exp, handler) { let vm = this;</p><pre><code>// exp - watch 
+// watch\u5C5E\u6027\u7684watcher
+new Watcher(vm, exp, handler, {
+  user: true
+});
+</code></pre><p>}</p><p>\u5728watcher\u91CC\uFF0C\u83B7\u53D6\u5230\u6539\u53D8\u524D\u4E0E\u6539\u53D8\u540E\u7684\u503C\uFF0C\u6267\u884C\u56DE\u8C03\u51FD\u6570\u3002</p><p>\u8FD9\u91CC\u7684watcher\u5C5E\u4E8E\u7528\u6237\u81EA\u5B9A\u4E49watcher\u3002</p><h2 id="\u8BA1\u7B97\u5C5E\u6027\u7684\u5B9E\u73B0" tabindex="-1">\u8BA1\u7B97\u5C5E\u6027\u7684\u5B9E\u73B0 <a class="header-anchor" href="#\u8BA1\u7B97\u5C5E\u6027\u7684\u5B9E\u73B0" aria-hidden="true">#</a></h2><p>\u6BCF\u4E00\u4E2A\u8BA1\u7B97\u5C5E\u6027\u90FD\u662F\u4E00\u4E2Awatcher\u3002</p><pre><code>function initComputed (vm, computed) {
+  let watchers = vm._watchersComputed = Object.create(null);
+
+  for (let key in computed) {
+    let useDef = computed[key];
+    // \u8BA1\u7B97\u5C5E\u6027\u7684watcher
+    watchers[key] = new Watcher(vm, useDef, () =&gt; {}, {
+      lazy: true
+    });
+    // \u8BBE\u7F6Egetter\u65B9\u6CD5
+    Object.defineProperty(vm, key, {
+      get: createComputedGetter(vm, key)
+    });
+  }
+}
+
+/**
+ * @description \u8BA1\u7B97\u5C5E\u6027\u7684getter\u65B9\u6CD5
+ * @param {object} vm - vue\u5B9E\u4F8B
+ * @param {string} key - \u952E
+ */
+function createComputedGetter (vm, key) {
+  let watcher = vm._watchersComputed[key];
+
+  // dirty -&gt; \u8BA1\u7B97\u5C5E\u6027\u7F13\u5B58\u95EE\u9898
+  return function () {
+    if (watcher) {
+      if (watcher.dirty) {
+        watcher.evaluate();
+      }
+      return watcher.value;
+    }
+  }
+}
+</code></pre><h2 id="\u8BA1\u7B97\u5C5E\u6027\u66F4\u65B0\u95EE\u9898" tabindex="-1">\u8BA1\u7B97\u5C5E\u6027\u66F4\u65B0\u95EE\u9898 <a class="header-anchor" href="#\u8BA1\u7B97\u5C5E\u6027\u66F4\u65B0\u95EE\u9898" aria-hidden="true">#</a></h2><p>\u4E3A\u8BA1\u7B97\u5C5E\u6027\u6DFB\u52A0\u6E32\u67D3watcher\u3001</p><p>/**</p><ul><li>@description \u8BA1\u7B97\u5C5E\u6027\u7684getter\u65B9\u6CD5</li><li>@param {object} vm - vue\u5B9E\u4F8B</li><li>@param {string} key - \u952E */ function createComputedGetter (vm, key) { let watcher = vm._watchersComputed[key];</li></ul><pre><code>// dirty -&gt; \u8BA1\u7B97\u5C5E\u6027\u7F13\u5B58\u95EE\u9898
+return function () {
+  if (watcher) {
+    if (watcher.dirty) {
+      watcher.evaluate();
+    }
+
+    if (Dep.target) {
+      watcher.depend();
+    }
+
+    return watcher.value;
+  }
+}
+</code></pre><p>}</p><p>/**</p><ul><li>@description computed\u5C5E\u6027\u66F4\u65B0</li><li>\u5C06\u6E32\u67D3watcher\u6DFB\u52A0\u5230\u8BA1\u7B97\u5C5E\u6027\u4E2D\u7684deps</li><li>\u7528\u4E8E\u8BA1\u7B97\u5C5E\u6027watcher\u51FA\u6808\u540E\u7684\u6570\u636E\u66F4\u65B0\u64CD\u4F5C */ depend () { let i = this.deps.length;</li></ul><pre><code>while (i--) {
+  this.deps[i].depend();
+}
+</code></pre><p>}</p><p>/**</p><ul><li>@description \u66F4\u65B0</li><li>@return {void} */ update () { if (this.lazy) { // \u8BA1\u7B97\u5C5E\u6027watcher\uFF0C\u91CD\u65B0\u53D6\u503C this.dirty = true; } else { // this.get(); // \u7ACB\u5373\u66F4\u65B0 -&gt; \u66F4\u65B0\u961F\u5217\u5904\u7406 queryWatcher(this); } }</li></ul><p>\u8BA1\u7B97\u5C5E\u6027\u66F4\u65B0\u76F8\u5BF9\u6BD4\u8F83\u590D\u6742\u3002</p><h2 id="\u4E86\u89E3\u865A\u62DFdom" tabindex="-1">\u4E86\u89E3\u865A\u62DFDOM <a class="header-anchor" href="#\u4E86\u89E3\u865A\u62DFdom" aria-hidden="true">#</a></h2><pre><code>\u865A\u62DFDOM\u5C31\u662F\u4E00\u4E2A\u5BF9\u8C61\u3002
+
+{
+  tag: div,
+  props: {},
+  children: [
+    {
+      tag: undefined,
+      props: undefined,
+      children: undefined,
+      text: &#39;yueluo&#39;
+    }
+  ]
+}
+
+\u4F7F\u7528\u865A\u62DFDOM\uFF0C\u53EF\u4EE5\u6709\u6548\u51CF\u5C11\u6027\u80FD\u6D88\u8017\u3002
+
+vue\u4E2D\uFF0C\u865A\u62DFDOM\u7684\u521B\u5EFA\u90FD\u662F\u4F9D\u9760h\u51FD\u6570\u3002
+
+\`\`\`js
+  new Vue({
+    render (h) {
+      return h(&#39;div&#39;, {}, &#39;yueluo&#39;);
+    }
+  })
+\`\`\`
+
+h\u7C7B\u4F3C\u4E8Ereact\u4E2D\u7684createElement\u65B9\u6CD5\u3002
+
+1. \u521B\u5EFA\u865A\u62DF\u8282\u70B9
+2. \u5C06\u865A\u62DF\u8282\u70B9\u8F6C\u6362\u4E3A\u771F\u5B9E\u8282\u70B9
+3. \u653E\u5165\u5230DOM\u4E2D
+</code></pre><h2 id="\u5C06\u865A\u62DFdom\u8F6C\u6362\u4E3A\u771F\u5B9Edom" tabindex="-1">\u5C06\u865A\u62DFDOM\u8F6C\u6362\u4E3A\u771F\u5B9EDOM <a class="header-anchor" href="#\u5C06\u865A\u62DFdom\u8F6C\u6362\u4E3A\u771F\u5B9Edom" aria-hidden="true">#</a></h2><pre><code>\u865A\u62DFDOM\u521B\u5EFA\u3001\u9996\u6B21\u6E32\u67D3
+
+\u9700\u8981\u5BF9\u65B0\u8282\u70B9\u548C\u65E7\u8282\u70B9\u8FDB\u884C\u6BD4\u5BF9\uFF0C\u7136\u540E\u5728\u66F4\u65B0\u9875\u9762\u3002
+
+\u64CD\u4F5C\u865A\u62DFDOM\uFF0C\u6BD4\u76F4\u63A5\u64CD\u4F5CDOM\u6027\u80FD\u597D\u5F97\u591A\u3002
+</code></pre><h2 id="\u8282\u70B9\u66F4\u65B0\u65F6\uFF0C\u865A\u62DFdom\u5904\u7406" tabindex="-1">\u8282\u70B9\u66F4\u65B0\u65F6\uFF0C\u865A\u62DFDOM\u5904\u7406 <a class="header-anchor" href="#\u8282\u70B9\u66F4\u65B0\u65F6\uFF0C\u865A\u62DFdom\u5904\u7406" aria-hidden="true">#</a></h2><pre><code>\u5B9E\u73B0patch\u65B9\u6CD5\u3002
+
+\u9488\u5BF9 \u8001\u8282\u70B9\u6709\u5B50\u8282\u70B9\uFF0C\u65B0\u8282\u70B9\u4E5F\u6709\u5B50\u8282\u70B9 \u7684\u60C5\u51B5\u8FDB\u884C\u7279\u6B8A\u5904\u7406\u3002
+</code></pre><h2 id="\u8001\u8282\u70B9\u6709\u5B50\u8282\u70B9\uFF0C\u65B0\u8282\u70B9\u4E5F\u6709\u5B50\u8282\u70B9\u7684\u60C5\u51B5" tabindex="-1">\u8001\u8282\u70B9\u6709\u5B50\u8282\u70B9\uFF0C\u65B0\u8282\u70B9\u4E5F\u6709\u5B50\u8282\u70B9\u7684\u60C5\u51B5 <a class="header-anchor" href="#\u8001\u8282\u70B9\u6709\u5B50\u8282\u70B9\uFF0C\u65B0\u8282\u70B9\u4E5F\u6709\u5B50\u8282\u70B9\u7684\u60C5\u51B5" aria-hidden="true">#</a></h2><pre><code>updateChildren DOM Diff \u6838\u5FC3\u903B\u8F91
+
+\u5904\u7406\u8FFD\u52A0\u5143\u7D20\u7684\u903B\u8F91\u3002
+
+a b c
+a b c d
+
+\`\`\`js
+/**
+ * @description \u66F4\u65B0\u5B50\u8282\u70B9
+ * @param {object} parent \u7236\u8282\u70B9
+ * @param {object} oldChildren \u65E7\u5B50\u8282\u70B9
+ * @param {object} newChildren \u65B0\u5B50\u8282\u70B9
+ * @return {void}
+ */
+function updateChildren (parent, oldChildren, newChildren) {
+  let oldStartIndex = 0,
+      oldStartVnode = oldChildren[0],
+      oldEndIndex = oldChildren.length - 1,
+      oldEndVnode = oldChildren[oldEndIndex];
+
+  let newStartIndex = 0,
+      newStartVnode = newChildren[0],
+      newEndIndex = newChildren.length - 1,
+      newEndVnode = newChildren[newEndIndex];
+
+  while (oldStartIndex &lt;= oldEndIndex &amp;&amp; newStartIndex &lt;= newEndIndex) {
+    if (isSameVnode(oldStartVnode, newStartVnode)) {
+      patch(oldStartVnode, newStartVnode);
+      oldStartVnode = oldChildren[++oldStartIndex];
+      newStartVnode = newChildren[++newStartIndex];
+    }
+  }
+
+  if (newStartIndex &lt;= newEndIndex) {
+    for (let i = newStartIndex; i &lt;= newEndIndex; i++) {
+      parent.appendChild(createElm(newChildren[i]));
+    }
+  }
+}
+\`\`\`
+
+\`\`\`js
+/**
+ * @description \u5224\u65AD\u662F\u4E0D\u662F\u540C\u4E00\u4E2A\u8282\u70B9
+ * @param {object} oldVnode \u8001\u8282\u70B9
+ * @param {object} newVnode \u65B0\u8282\u70B9
+ * @return {void}
+ */
+function isSameVnode (oldVnode, newVnode) {
+  return (oldVnode.tag === newVnode.tag) &amp;&amp; (oldVnode.key === newVnode.key);
+}
+\`\`\`
+</code></pre><h2 id="\u5904\u7406\u65B0\u5143\u7D20\u5728\u5934\u90E8\u6216\u5143\u7D20\u5012\u5E8F\u95EE\u9898" tabindex="-1">\u5904\u7406\u65B0\u5143\u7D20\u5728\u5934\u90E8\u6216\u5143\u7D20\u5012\u5E8F\u95EE\u9898 <a class="header-anchor" href="#\u5904\u7406\u65B0\u5143\u7D20\u5728\u5934\u90E8\u6216\u5143\u7D20\u5012\u5E8F\u95EE\u9898" aria-hidden="true">#</a></h2><pre><code>1. 
+
+  a b c
+  e d a b c
+
+2.
+
+  a b c
+  c b a
+
+\`\`\`js
+/**
+ * @description \u66F4\u65B0\u5B50\u8282\u70B9
+ * @param {object} parent \u7236\u8282\u70B9
+ * @param {object} oldChildren \u65E7\u5B50\u8282\u70B9
+ * @param {object} newChildren \u65B0\u5B50\u8282\u70B9
+ * @return {void}
+ */
+function updateChildren (parent, oldChildren, newChildren) {
+  let oldStartIndex = 0,
+      oldStartVnode = oldChildren[0],
+      oldEndIndex = oldChildren.length - 1,
+      oldEndVnode = oldChildren[oldEndIndex];
+
+  let newStartIndex = 0,
+      newStartVnode = newChildren[0],
+      newEndIndex = newChildren.length - 1,
+      newEndVnode = newChildren[newEndIndex];
+
+  while (oldStartIndex &lt;= oldEndIndex &amp;&amp; newStartIndex &lt;= newEndIndex) {
+    if (isSameVnode(oldStartVnode, newStartVnode)) {
+      patch(oldStartVnode, newStartVnode);
+      oldStartVnode = oldChildren[++oldStartIndex];
+      newStartVnode = newChildren[++newStartIndex];
+    } else if (isSameVnode(oldEndVnode, newEndVnode)) {
+      patch(oldEndVnode, newEndVnode);
+      oldEndVnode = oldChildren[--oldEndIndex];
+      newEndVnode = newChildren[--newEndIndex];
+    } else if (isSameVnode(oldStartVnode, newEndVnode)) {
+      patch(oldStartVnode, newEndVnode);
+      parent.insertBefore(oldStartVnode.el, oldEndVnode.el.nextSibling);
+      oldStartVnode = oldChildren[++oldStartIndex];
+      newEndVnode = newChildren[--newEndIndex];
+    }
+  }
+
+  if (newStartIndex &lt;= newEndIndex) {
+    for (let i = newStartIndex; i &lt;= newEndIndex; i++) {
+      let refElm = newChildren[newEndIndex + 1].el;
+      parent.insertBefore(createElm(newChildren[i]), refElm);
+    }
+  }
+}
+\`\`\`
+
+\u4E3A\u4EC0\u4E48\u7ECF\u5E38\u8BF4\u7ED9\u5C5E\u6027\u8BBE\u7F6Ekey\u503C\u65F6\uFF0C\u4E0D\u8981\u7528\u7D22\u5F15\uFF1F
+
+  a b c d
+  d c b a
+
+  \u5982\u679C\u5143\u7D20\u9996\u5C3E\u98A0\u5012\u65F6\uFF0C\u7D22\u5F15\u662F\u4E0D\u53D8\u7684\uFF0Cdiff\u7B97\u6CD5\u6BD4\u8F83\u65F6\uFF0C\u4F1A\u6839\u636E\u7D22\u5F15\u53BB\u67E5\u8BE2\u5143\u7D20\u3002
+  \u5BFC\u81F4\u4F1A\u88AB\u5224\u65ADn\u6B21\uFF0C\u5982\u679C\u4E0D\u4F7F\u7528\u7D22\u5F15\uFF0C\u53EA\u4F1A\u865A\u62DFDOM\u5143\u7D20\u7684\u79FB\u52A8\u3002
+
+  \u5982\u679C\u4E0D\u4F7F\u7528\u7D22\u5F15\u4F5C\u4E3Akey\uFF0C\u53EA\u9700\u89813\u6B21DOM\u79FB\u52A8\uFF0C\u5012\u5E8F\u7684\u65F6\u5019\u53EF\u4EE5\u51CF\u5C11\u6027\u80FD\u6D88\u8017\u3002
+  \u5982\u679C\u4F7F\u7528\u7D22\u5F15\u4F5C\u4E3Akey\uFF0C\u4F1A\u8FDB\u884C4\u6B21\u91CD\u65B0\u6E32\u67D3\u624D\u5F62\u6210\u6700\u7EC8\u7684\u7ED3\u679C\u3002
+</code></pre><h2 id="\u865A\u62DFdom\u4EE3\u7801\u5408\u5E76\u5230\u539F\u6709vue\u903B\u8F91\u4E2D\uFF08\u5408\u5E76vue\u4EE3\u7801" tabindex="-1">\u865A\u62DFDOM\u4EE3\u7801\u5408\u5E76\u5230\u539F\u6709vue\u903B\u8F91\u4E2D\uFF08\u5408\u5E76Vue\u4EE3\u7801) <a class="header-anchor" href="#\u865A\u62DFdom\u4EE3\u7801\u5408\u5E76\u5230\u539F\u6709vue\u903B\u8F91\u4E2D\uFF08\u5408\u5E76vue\u4EE3\u7801" aria-hidden="true">#</a></h2><pre><code>\u5C06\u865A\u62DFDOM\u5408\u5E76\u5230\u539F\u6709\u903B\u8F91\u4E2D\u3002
+</code></pre><h1 id="vue\u6E90\u7801" tabindex="-1">Vue\u6E90\u7801 <a class="header-anchor" href="#vue\u6E90\u7801" aria-hidden="true">#</a></h1><p>package.json</p><pre><code>\`\`\`js
+&quot;main&quot;: &quot;dist/vue.runtime.common.js&quot; =&gt; require(&#39;vue&#39;); common.js
+&quot;module&quot;: &quot;dist/vue.runtime.esm.js&quot; =&gt; import Vue from &#39;vue&#39;; es module
+\`\`\`
+
+\`\`\`js
+&quot;scipts&quot;: {
+  &quot;dev&quot;: &quot;rollup -w -c scripts/config.js --environment TAGHET:web-full-dev&quot;,
+  &quot;build&quot;: &quot;node script/build.js&quot;,
+  &quot;build:ssr&quot;: &quot;npm run build -- web-runtime-cjs,web-server-renderer&quot;,
+  &quot;build:weex&quot;: &quot;npm run build -- weex&quot;
+}
+\`\`\`
+</code></pre><p>scripts/build.js</p><pre><code>...
+</code></pre>`,64),i=[a];function o(l,h,c,u,p,s){return t(),n("div",null,i)}var v=e(r,[["render",o]]);export{w as __pageData,v as default};
