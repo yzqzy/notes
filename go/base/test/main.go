c6f52167683f116300de01cdf595005249c906bb
heora <yangzhiqiang@winwin-inc.com>
2023-01-24 09:29:04 +0800

    docs: 代码块与作用域

--- go/base/test/main.go
+++ go/base/test/main.go
@@ -1,10 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func init() {
-	fmt.Println("init invoked")
+var a int = 2020
+
+func checkYear() error {
+	err := errors.New("wrong year")
+
+	switch a, err := getYear(); a {
+	case 2020:
+		fmt.Println("it is", a, err)
+	case 2021:
+		fmt.Println("it is", a)
+		err = nil
+	}
+	fmt.Println("after check, it is", a)
+	return err
+}
+
+type new int
+
+func getYear() (new, error) {
+	var b int16 = 2021
+	return new(b), nil
 }
 
 func main() {
+	err := checkYear()
+	if err != nil {
+		fmt.Println("call checkYear error:", err)
+		return
+	}
+	fmt.Println("call checkYear ok")
 }
