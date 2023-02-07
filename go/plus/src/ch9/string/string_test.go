b83cd81d6ff0ee19348b50ec63d239e4f29c2f34
heora <yueluo.yang@gmail.com>
2023-02-07 08:21:10 +0800

    feat(go): strings

---
+++ go/plus/src/ch9/string/string_test.go
@@ -0,0 +1,29 @@
+package string_test
+
+import (
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	var s string
+	t.Log(s) // 初始化为默认零值
+
+	s = "hello"
+	t.Log(len(s)) // 5
+
+	// string 是不可变的 byte 切片
+	// s[1] = '3' // cannot assign to s[1] (value of type byte)
+
+	s = "\xE4\xB8\xA5" // 可以存储二进制数据
+	t.Log(s)           // 严
+
+	s = "中"
+	t.Log(len(s)) // 3 存储的是 byte 数
+
+	c := []rune(s)
+	t.Log(len(c)) // 1
+	// t.Log("run size:", unsafe.Sizeof(c[0])) // run size: 4
+
+	t.Logf("中 unicode %x", c[0]) // 中 unicode 4e2d
+	t.Logf("中 utf8 %x", s)       // 中 utf8 e4b8ad
+}
