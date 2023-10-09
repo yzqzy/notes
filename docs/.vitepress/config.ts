c336b28a7e6d919add096a6d1fb34c2ed4964720
heora <yangzhiqiang@winwin-inc.com>
2023-10-09 18:24:48 +0800

    fix: upgrade vitepress version

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,19 +1,17 @@
-import { defineConfig } from 'vitepress'
 import sidebars from './sidebar.json'
 
-// @ts-ignore
-export default defineConfig(ctx => ({
+export default {
   lang: 'en-US',
   title: 'Personal Notes',
   titleTemplate: '月落 - Personal Notes',
   head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
   description: 'Web Developer & JS Fancier',
   themeConfig: {
-    sidebar: sidebars as any,
-    socialLinks: [{ icon: 'github', link: 'https://github.com/yw0525' }],
+    sidebar: sidebars,
+    socialLinks: [{ icon: 'github', link: 'https://github.com/yzqzy' }],
     footer: {
-      copyright: 'Copyright © 2022-月落 版权所有'
+      copyright: 'Copyright © 2023-月落 版权所有'
     },
     outline: 'deep'
   }
-}))
+}
